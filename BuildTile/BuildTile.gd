933743b84c4344980b4c20397d9b2a9962d60710
SpaceAgeX <aaryeh27@palmertrinity.org>
2024-03-05 18:56:38 -0500

    Added Limits to Building

--- BuildTile/BuildTile.gd
+++ BuildTile/BuildTile.gd
@@ -11,6 +11,7 @@ enum Types{
 	
 }
 
+var placeable = true
 var InArea = false
 
 @export var type:Types = Types.EMPTY
@@ -26,22 +27,27 @@ func _physics_process(delta):
 		print("Click on:"+str(position))
 		clicked.emit(self.name)
 		
+		
 
 
 func updateType():
-	match type:
-		Types.EMPTY:
-			Building.visible = false
-		Types.TOWN:
-			Building.visible = true
-			Building.frame = 0
-		Types.FACTORY:
-			Building.visible = true
-			Building.frame = 3
-		Types.SILO:
-			Building.visible = true
-			Building.frame = 6
-			
+	if placeable:
+		match type:
+			Types.EMPTY:
+				Building.visible = false
+			Types.TOWN:
+				Building.visible = true
+				Building.frame = 0
+				placeable = false
+			Types.FACTORY:
+				Building.visible = true
+				Building.frame = 3
+				placeable = false
+			Types.SILO:
+				Building.visible = true
+				Building.frame = 6
+				placeable = false
+				
 func _on_area_2d_mouse_entered():
 	InArea = true
 

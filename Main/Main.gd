f20e5a9d876762c289f1dfc65fa1b9908e984c61
SpaceAgeX <aaryeh27@palmertrinity.org>
2024-03-08 07:51:15 -0500

    Placing Mechanics Improvement

--- Main/Main.gd
+++ Main/Main.gd
@@ -19,10 +19,18 @@ func _ready():
 	for x in $Buildings.get_children():
 		x.clicked.connect(on_clicked)
 		
+func _physics_process(delta):
+	if Input.is_action_just_pressed("RightClick"):
+		$UI/Buttons.visible = true
+		on = Buttons.EMPTY
+		cancel_place()
 		
+func cancel_place():
+	pass
 
 func on_clicked(tile):
 	Building = get_node("Buildings/"+str(tile))
+	
 	if Building.placeable:
 		print(tile)
 		print(on)
@@ -30,6 +38,7 @@ func on_clicked(tile):
 			Buttons.EMPTY:
 				Building.type = Building.Types.EMPTY
 				on = Buttons.EMPTY
+				
 			Buttons.TOWN:
 				Building.type = Building.Types.TOWN
 				on = Buttons.EMPTY
@@ -39,21 +48,26 @@ func on_clicked(tile):
 			Buttons.SILO:
 				Building.type = Building.Types.SILO
 				on = Buttons.EMPTY
+		$UI/Buttons.visible = true
 	else:
 		$UI/HUD/Display.write("Can't Place There", 3)
-		
+	
+	
 	get_node("Buildings/"+str(tile)).updateType()
 
 
 
 
 func _on_town_pressed():
+	$UI/Buttons.visible = false
 	on = Buttons.TOWN
 
 
 func _on_factory_pressed():
+	$UI/Buttons.visible = false
 	on = Buttons.FACTORY
 
 
 func _on_silo_pressed():
+	$UI/Buttons.visible = false
 	on = Buttons.SILO
